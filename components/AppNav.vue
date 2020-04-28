41a98f1154b524ccace464bdf83efe8fa9b6c675
theGreatCornholio <faust.child@gmail.com>
2020-04-28 16:54:58 +0300

    Header nav update

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -56,6 +56,7 @@ export default {
     return {
       navItems: [
         { text: 'Home', to: { name: 'index' }, isInnerLink: true },
+        { text: 'Usage', to: { name: 'index', hash: '#startUsing' }, isInnerLink: true },
         { text: 'Service', to: { name: 'index', hash: '#service' }, isInnerLink: true },
         { text: 'About', to: { name: 'index', hash: '#about' }, isInnerLink: true },
         { text: 'Blog', to: 'https://medium.com/@tezosukraine', isInnerLink: false },
